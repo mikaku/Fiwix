9991c8aa17e822b6af28c85ad896696c3c343682
Jordi Sanfeliu <jordi@fibranet.cat>
2021-02-15 09:43:05 +0100

    updated the header file to support the latest GRUB Multihost Specification v1

--- include/fiwix/bios.h
+++ include/fiwix/bios.h
@@ -8,12 +8,8 @@
 #ifndef _FIWIX_BIOS_H
 #define _FIWIX_BIOS_H
 
-#include <fiwix/multiboot.h>
+#include <fiwix/multiboot1.h>
 
-#define BIOS_MEM_AVAIL		1	/* BIOS memory available */
-#define BIOS_MEM_RES		2	/* BIOS memory reserved */
-#define BIOS_MEM_ACPI_REC	3	/* BIOS memory ACPI reclaim */
-#define BIOS_MEM_ACPI_NVS	4	/* BIOS memory ACPI NVS */
 #define NR_BIOS_MM_ENT		25	/* entries in BIOS memory map */
 
 struct bios_mem_map {
@@ -24,6 +20,6 @@ struct bios_mem_map {
 struct bios_mem_map bios_mem_map[NR_BIOS_MM_ENT];
 
 int addr_in_bios_map(unsigned int);
-void bios_map_init(memory_map_t *, unsigned long int);
+void bios_map_init(struct multiboot_mmap_entry *, unsigned long int);
 
 #endif /* _FIWIX_BIOS_H */
