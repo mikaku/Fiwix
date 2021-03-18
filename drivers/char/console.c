51457bd3573279da2d1a0d04254c46917f3e4b5d
Jordi Sanfeliu <jordi@fibranet.cat>
2021-03-18 19:06:26 +0100

    fixed the CSI J Erase in Display (ED) sequence in console

--- drivers/char/console.c
+++ drivers/char/console.c
@@ -178,7 +178,7 @@ static void csi_J(struct vconsole *vc, int mode)
 
 	switch(mode) {
 		case CSI_J_CUR2END:	/* Erase Down <ESC>[J */
-			from = ((vc->y * vc->columns) + vc->x) * 2;
+			from = (vc->y * vc->columns) + vc->x;
 			count = (SCREEN_SIZE - from) / sizeof(short int);
 			break;
 		case CSI_J_STA2CUR:	/* Erase Up <ESC>[1J */
