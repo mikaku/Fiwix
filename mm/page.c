mikaku/Fiwix

commit b13601c2c0d670b10de39d89b8a2651d00367542

more readable code

mm/page.c
@@ -377,10 +377,9 @@ int file_read(struct inode *i, struct fd *fd_table, char *buffer, __size_t count
 				printk("%s(): returning -ENOMEM\n", __FUNCTION__);
 				return -ENOMEM;
 			}
-			page = V2P(page);
-			pg = &page_table[page >> PAGE_SHIFT];
+			pg = &page_table[V2P(page) >> PAGE_SHIFT];
 			if(bread_page(pg, i, fd_table->offset & PAGE_MASK, 0, MAP_SHARED)) {
-				kfree((unsigned int)pg->data);
+				kfree(page);
 				inode_unlock(i);
 				printk("%s(): returning -EIO\n", __FUNCTION__);
 				return -EIO;
@@ -397,7 +396,7 @@ int file_read(struct inode *i, struct fd *fd_table, char *buffer, __size_t count
 		poffset %= PAGE_SIZE;
 		fd_table->offset += bytes;
 		page_unlock(pg);
-		kfree((unsigned int)pg->data);
+		kfree(page);
 	}
 
 	inode_unlock(i);

commit a7a409dd296b80f8e4bd975cba4d9e3f22656236

fixed a bug introduced in b13601c

mm/page.c
@@ -384,6 +384,8 @@ int file_read(struct inode *i, struct fd *fd_table, char *buffer, __size_t count
 				printk("%s(): returning -EIO\n", __FUNCTION__);
 				return -EIO;
 			}
+		} else {
+			page = (unsigned int)pg->data;
 		}
 
 		page_lock(pg);