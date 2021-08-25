bce34f09bd1c537c7ce092a25b13c5a0b4cc312b
Jordi Sanfeliu <jordi@fibranet.cat>
2021-08-25 14:05:36 +0200

    updated the copyright year

--- kernel/syscalls/chown.c
+++ kernel/syscalls/chown.c
@@ -1,7 +1,7 @@
 /*
  * fiwix/kernel/syscalls/chown.c
  *
- * Copyright 2018, Jordi Sanfeliu. All rights reserved.
+ * Copyright 2018-2021, Jordi Sanfeliu. All rights reserved.
  * Distributed under the terms of the Fiwix License.
  */
 
--- kernel/syscalls/open.c
+++ kernel/syscalls/open.c
@@ -1,7 +1,7 @@
 /*
  * fiwix/kernel/syscalls/open.c
  *
- * Copyright 2018, Jordi Sanfeliu. All rights reserved.
+ * Copyright 2018-2021, Jordi Sanfeliu. All rights reserved.
  * Distributed under the terms of the Fiwix License.
  */
 
