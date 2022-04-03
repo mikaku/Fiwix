19788e7d4045d5fd826974b25166a3bc44e18254
Alwin Berger <alwin.berger@tu-dortmund.de>
2022-04-03 09:53:05 +0200

    getcwd: simplify and formatting
    Changes to the control flow as suggested by Mikaku.
    Formatting brought in line with coding standards.
    A write to buf[size] was fixed.

--- kernel/syscalls/getcwd.c
+++ kernel/syscalls/getcwd.c
@@ -2,6 +2,7 @@
  * fiwix/kernel/syscalls/getcwd.c
  *
  * Copyright 2018, Jordi Sanfeliu. All rights reserved.
+ * Copyright 2022, Alwin Berger. All rights reserved.
  * Distributed under the terms of the Fiwix License.
  */
 
@@ -27,60 +28,37 @@ int sys_getcwd(char *buf, __size_t size)
 	if((errno = check_user_area(VERIFY_WRITE, buf, size))) {
 		return errno;
 	}
-	/* Psedocode:
-	 * dir <- current->pwd
-	 * if dir == /
-	 *	return "/"
-	 * cwd <- ""
-	 * while dir != /
-	 * 	p <- lookup(..)
-	 *	open(p)
-	 *	dirent <- read(p)
-	 *	for d in dirent
-	 *		if d == dir
-	 *			cwd <- '/' + d.name + cwd
-	 *			dir <- d
-	 *			break
-	 * return cwd
-	 */
-	 if (size==0 || buf==NULL) {    /* buffer self allocation not supported */
-		 return -EINVAL;
-	 }
-	 if (size<2) { /* the shortest path is "/"" */
-		 return -ERANGE;
-	 }
-	struct dirent* dirent_buf;
-	struct dirent* d_ptr;
-	struct inode* cur = current->pwd;
-	struct inode* up = cur;
-	struct inode* root;
-	struct inode* tmp_ino = NULL;
-	int tmp_fd;
-	int bytes_read;
-	int done = 0;
-	__size_t marker = size-2;	/* Reserve '\0' at the end */
-	__size_t namelength;
-	buf[size-1] = 0;
-	char save;
-	__size_t x;
-	if((errno = namei("/",&root,0,FOLLOW_LINKS))) {
-		return errno;
+	/* Buffer self-allocation not supported. */
+	if (size==0 || buf==NULL) {
+		return -EINVAL;
 	}
-	if (cur == root) {
-		/* This case needs special handling, otherwise the loop skips over root */
+	/* The shortest possible path is "/". */
+	if (size<2) {
+		return -ERANGE;
+	}
+	struct dirent *dirent_buf;
+	struct dirent *d_ptr;
+	struct inode *cur = current->pwd;
+	struct inode *up = cur;
+	struct inode *tmp_ino = NULL;
+	int tmp_fd, done;
+	int namelength, bytes_read;
+	__size_t marker = size-2;	/* Reserve '\0' at the end. */
+	buf[size-1] = 0;
+	__size_t x;
+
+	if(cur == current->root) {
+		/* This case needs special handling, otherwise the loop skips over root. */
 		buf[0]='/';
 		buf[1]='\0';
-		iput(root);
 		return (unsigned int)buf;
 	}
 	if(!(dirent_buf = (void *)kmalloc())) {
-		iput(root);
 		return -ENOMEM;
 	}
 
 	do {
-		if((errno = parse_namei("..",cur,&up,0,FOLLOW_LINKS))) {
-			iput(root);
+		if((errno = parse_namei("..", cur, &up, 0, FOLLOW_LINKS))) {
 			if (cur != current->pwd) {
 				iput(cur);
 			}
@@ -88,7 +66,6 @@ int sys_getcwd(char *buf, __size_t size)
 			return errno;
 		}
 		if((tmp_fd = get_new_fd(up)) < 0) {
-			iput(root);
 			iput(up);
 			if (cur != current->pwd) {
 				iput(cur);
@@ -97,12 +74,10 @@ int sys_getcwd(char *buf, __size_t size)
 			return tmp_fd;
 		}
 		do {
-			done=0;
-			bytes_read = up->fsop->readdir(up, &fd_table[tmp_fd],
-					dirent_buf, PAGE_SIZE);
+			done = 0;
+			bytes_read = up->fsop->readdir(up, &fd_table[tmp_fd], dirent_buf, PAGE_SIZE);
 			if (bytes_read < 0) {
 				release_fd(tmp_fd);
-				iput(root);
 				iput(up);
 				if (cur != current->pwd) {
 					iput(cur);
@@ -112,88 +87,57 @@ int sys_getcwd(char *buf, __size_t size)
 			}
 			d_ptr = dirent_buf;
 			while((void *) d_ptr < ((void *) dirent_buf + bytes_read)) {
-				if(up->dev==cur->dev) {
-					if(d_ptr->d_ino==cur->inode) {
-						if(d_ptr->d_ino == cur->inode) {
-							namelength = strlen(d_ptr->d_name);
-							if (marker < namelength+1) {
-								release_fd(tmp_fd);
-								iput(root);
-								iput(up);
-								if (cur != current->pwd) {
-									iput(cur);
-								}
-								kfree((unsigned int)dirent_buf);
-								return -ERANGE;
-							}
-							marker -= namelength+1;	/* +1 for the leading '/' */
-							save=buf[marker+namelength+1];
-							strncpy(buf+marker+1, d_ptr->d_name, namelength);
-							buf[marker] = '/';
-							buf[marker+namelength+1] = save; /* strncp overrides '/' or '\0' */
-							done=1;
-							break;
-						}
-					}
+				if((errno = parse_namei(d_ptr->d_name, up, &tmp_ino, 0, FOLLOW_LINKS))) {
+					/* Keep going if sibling dirents fail. */
+					break;
 				}
-				else if (strcmp(".",d_ptr->d_name) != 0 && strcmp("..",d_ptr->d_name) != 0)
-				{	/* Need to deal with mounts */
-					if((errno = parse_namei(d_ptr->d_name,up,&tmp_ino,0,FOLLOW_LINKS))) {
-						break;	/* Keep going if sibling dirents fail */
-					}
-					if(tmp_ino->inode==cur->inode) {
+				if(tmp_ino->inode == cur->inode) {
+					if(strcmp("..", d_ptr->d_name)) {
 						namelength = strlen(d_ptr->d_name);
 						if (marker < namelength+1) {
 							release_fd(tmp_fd);
-							iput(root);
 							iput(up);
-							iput(tmp_ino);
 							if (cur != current->pwd) {
 								iput(cur);
 							}
 							kfree((unsigned int)dirent_buf);
 							return -ERANGE;
 						}
-						marker -= namelength+1;	/* +1 for the leading '/' */
-						save=buf[marker+namelength+1];
-						strncpy(buf+marker+1, d_ptr->d_name, namelength);
-						buf[marker] = '/';
-						buf[marker+namelength+1] = save; /* strncp overrides '/' or '\0' */
-						done=1;
+						while(--namelength >= 0) {
+							buf[marker--] = d_ptr->d_name[namelength];
+						}
+						buf[marker--] = '/';
 						iput(tmp_ino);
+						done = 1;
 						break;
 					}
-					iput(tmp_ino);
 				}
-				d_ptr = (struct dirent *) ((void *)d_ptr+d_ptr->d_reclen);
+				d_ptr = (struct dirent *) ((void *)d_ptr + d_ptr->d_reclen);
+				iput(tmp_ino);
 			}
 		} while(bytes_read!=0 && !done);
 
-
 		release_fd(tmp_fd);
-		if (!done) {   /* parent dir was fully read, child still not found */
-			iput(root);
+		if(!done) {
+			/* Parent dir was fully read, child still not found. */
 			iput(up);
 			if (cur != current->pwd) {
 				iput(cur);
 			}
 			kfree((unsigned int)dirent_buf);
-			if (errno) {
-				return errno;		/* parse_namei failed on the right dirent */
-			} else {
-				return -ENOENT;
-			}
+			return -ENOENT;
 		}
 		if (cur != current->pwd) {
 			iput(cur);
 		}
 		cur = up;
-	} while(cur != root);
+	} while(cur != current->root);
+
 	kfree((unsigned int)dirent_buf);
-	iput(root); /* cur == up == root */
-	/* Move the String to the start of the buffer */
-	for ( x = marker; x < size; x++) {
-		buf[x-marker]=buf[x];
+	iput(cur);
+	/* Move the String to the start of the buffer. */
+	for (x = ++marker; x < size; x++) {
+		buf[x-marker] = buf[x];
 	}
 	return (unsigned int)buf;
 }
