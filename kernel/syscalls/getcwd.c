1ef63cf81e875f5304f4146faedec092cbe4fa1b
Alwin Berger <alwin.berger@tu-dortmund.de>
2022-04-03 16:23:59 +0200

    getcwd: improve coding style

--- kernel/syscalls/getcwd.c
+++ kernel/syscalls/getcwd.c
@@ -20,6 +20,16 @@
 int sys_getcwd(char *buf, __size_t size)
 {
 	int errno;
+	struct dirent *dirent_buf;
+	struct dirent *d_ptr;
+	struct inode *cur;
+	struct inode *up;
+	struct inode *tmp_ino;
+	int tmp_fd, done;
+	int namelength, bytes_read;
+	int diff_dev;
+	__size_t marker;
+	__size_t x;
 
 #ifdef __DEBUG__
 	printk("(pid %d) sys_getcwd(0x%08x, %d)\n", current->pid, (unsigned int)buf, size);
@@ -28,30 +38,24 @@ int sys_getcwd(char *buf, __size_t size)
 	if((errno = check_user_area(VERIFY_WRITE, buf, size))) {
 		return errno;
 	}
-	/* Buffer self-allocation not supported. */
-	if (size==0 || buf==NULL) {
+	/* buffer self-allocation not supported */
+	if(size == 0 || buf == NULL) {
 		return -EINVAL;
 	}
-	/* The shortest possible path is "/". */
-	if (size<2) {
+	/* the shortest possible path is "/" */
+	if(size < 2) {
 		return -ERANGE;
 	}
-	struct dirent *dirent_buf;
-	struct dirent *d_ptr;
-	struct inode *cur = current->pwd;
-	struct inode *up = cur;
-	struct inode *tmp_ino = NULL;
-	int tmp_fd, done;
-	int namelength, bytes_read;
-	int diff_dev;
-	__size_t marker = size-2;	/* Reserve '\0' at the end. */
-	buf[size-1] = 0;
-	__size_t x;
+
+	cur = current->pwd;
+	up = cur;
+	marker = size - 2;	/* reserve '\0' at the end */
+	buf[size - 1] = 0;
 
 	if(cur == current->root) {
-		/* This case needs special handling, otherwise the loop skips over root. */
-		buf[0]='/';
-		buf[1]='\0';
+		/* this case needs special handling, otherwise the loop skips over root */
+		buf[0] = '/';
+		buf[1] = '\0';
 		return (unsigned int)buf;
 	}
 	if(!(dirent_buf = (void *)kmalloc())) {
@@ -60,7 +64,7 @@ int sys_getcwd(char *buf, __size_t size)
 
 	do {
 		if((errno = parse_namei("..", cur, &up, 0, FOLLOW_LINKS))) {
-			if (cur != current->pwd) {
+			if(cur != current->pwd) {
 				iput(cur);
 			}
 			kfree((unsigned int)dirent_buf);
@@ -68,7 +72,7 @@ int sys_getcwd(char *buf, __size_t size)
 		}
 		if((tmp_fd = get_new_fd(up)) < 0) {
 			iput(up);
-			if (cur != current->pwd) {
+			if(cur != current->pwd) {
 				iput(cur);
 			}
 			kfree((unsigned int)dirent_buf);
@@ -77,10 +81,10 @@ int sys_getcwd(char *buf, __size_t size)
 		do {
 			done = 0;
 			bytes_read = up->fsop->readdir(up, &fd_table[tmp_fd], dirent_buf, PAGE_SIZE);
-			if (bytes_read < 0) {
+			if(bytes_read < 0) {
 				release_fd(tmp_fd);
 				iput(up);
-				if (cur != current->pwd) {
+				if(cur != current->pwd) {
 					iput(cur);
 				}
 				kfree((unsigned int)dirent_buf);
@@ -88,25 +92,27 @@ int sys_getcwd(char *buf, __size_t size)
 			}
 			d_ptr = dirent_buf;
 			while((void *) d_ptr < ((void *) dirent_buf + bytes_read)) {
-				/* If the child is on the same device as the parent the d_ptr->d_ino should be correct.
-				   In this case there is no need to call a namei again. */
+				/*
+				 * If the child is on the same device as the parent the d_ptr->d_ino should be correct.
+				 * In this case there is no need to call a namei again.
+				 */
 				diff_dev = up->dev != cur->dev;
-				if (diff_dev) {
+				if(diff_dev) {
 					if((errno = parse_namei(d_ptr->d_name, up, &tmp_ino, 0, FOLLOW_LINKS))) {
-						/* Keep going if sibling dirents fail. */
+						/* keep going if sibling dirents fail */
 						break;
 					}
 				}
-				if(d_ptr->d_ino == cur->inode && !diff_dev || tmp_ino->inode == cur->inode && diff_dev) {
+				if((d_ptr->d_ino == cur->inode && !diff_dev) || (tmp_ino->inode == cur->inode && diff_dev)) {
 					if(strcmp("..", d_ptr->d_name)) {
 						namelength = strlen(d_ptr->d_name);
-						if (marker < namelength+1) {
+						if(marker < namelength + 1) {
 							release_fd(tmp_fd);
 							iput(up);
-							if (cur != current->pwd) {
+							if(cur != current->pwd) {
 								iput(cur);
 							}
-							if (diff_dev) {
+							if(diff_dev) {
 								iput(tmp_ino);
 							}
 							kfree((unsigned int)dirent_buf);
@@ -116,31 +122,31 @@ int sys_getcwd(char *buf, __size_t size)
 							buf[marker--] = d_ptr->d_name[namelength];
 						}
 						buf[marker--] = '/';
-						if (diff_dev) {
+						if(diff_dev) {
 							iput(tmp_ino);
 						}
 						done = 1;
 						break;
 					}
 				}
-				if (diff_dev) {
+				if(diff_dev) {
 					iput(tmp_ino);
 				}
 				d_ptr = (struct dirent *) ((void *)d_ptr + d_ptr->d_reclen);
 			}
-		} while(bytes_read!=0 && !done);
+		} while(bytes_read != 0 && !done);
 
 		release_fd(tmp_fd);
 		if(!done) {
-			/* Parent dir was fully read, child still not found. */
+			/* parent dir was fully read, child still not found */
 			iput(up);
-			if (cur != current->pwd) {
+			if(cur != current->pwd) {
 				iput(cur);
 			}
 			kfree((unsigned int)dirent_buf);
 			return -ENOENT;
 		}
-		if (cur != current->pwd) {
+		if(cur != current->pwd) {
 			iput(cur);
 		}
 		cur = up;
@@ -148,9 +154,9 @@ int sys_getcwd(char *buf, __size_t size)
 
 	kfree((unsigned int)dirent_buf);
 	iput(cur);
-	/* Move the String to the start of the buffer. */
-	for (x = ++marker; x < size; x++) {
-		buf[x-marker] = buf[x];
+	/* move the string to the start of the buffer */
+	for(x = ++marker; x < size; x++) {
+		buf[x - marker] = buf[x];
 	}
 	return (unsigned int)buf;
 }
