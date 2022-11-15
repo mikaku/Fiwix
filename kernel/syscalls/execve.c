c34ad4793d2b7ceb6769ee3339b43523624fdc12
Jordi Sanfeliu <jordi@fibranet.cat>
2022-11-15 09:31:49 +0100

    changed sys_execve() to use the first argument instead of argv[0]

--- kernel/syscalls/execve.c
+++ kernel/syscalls/execve.c
@@ -353,7 +353,7 @@ int sys_execve(const char *filename, char *argv[], char *envp[], int arg4, int a
 	printk("(pid %d) sys_execve('%s', ...)\n", current->pid, filename);
 #endif /*__DEBUG__ */
 
-	if((errno = malloc_name(argv[0], &tmp_name)) < 0) {
+	if((errno = malloc_name(filename, &tmp_name)) < 0) {
 		return errno;
 	}
 	/* copy filename into kernel address space */
