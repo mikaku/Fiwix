mikaku/Fiwix

commit 9cfe3725e4c5488bcfd2da56ecb1a8587a4ae4b9

changed to use the doubly-linked list instead of the whole array in every single process search loop

kernel/syscalls/wait4.c
@@ -36,6 +36,7 @@ int sys_wait4(__pid_t pid, int *status, int options, struct rusage *ru)
 		flag = 0;
 		FOR_EACH_PROCESS(p) {
 			if(p->ppid != current->pid) {
+				p = p->next;
 				continue;
 			}
 			if(pid > 0) {
@@ -58,13 +59,12 @@ int sys_wait4(__pid_t pid, int *status, int options, struct rusage *ru)
 			}
 			if(flag) {
 				if(p->state == PROC_STOPPED) {
-					if(!p->exit_code) {
-						continue;
+					if(p->exit_code) {
+						if(status) {
+							*status = (p->exit_code << 8) | 0x7F;
+						}
+						p->exit_code = 0;
 					}
-					if(status) {
-						*status = (p->exit_code << 8) | 0x7F;
-					}
-					p->exit_code = 0;
 					if(ru) {
 						get_rusage(p, ru);
 					}
@@ -81,6 +81,7 @@ int sys_wait4(__pid_t pid, int *status, int options, struct rusage *ru)
 					return remove_zombie(p);
 				}
 			}
+			p = p->next;
 			flag = 0;
 		}
 		if(options & WNOHANG) {

drivers/char/sysrq.c
@@ -29,7 +29,7 @@ static void process_list(void)
 
 	printk("USER   PID   PPID  S  CMD\n");
 	FOR_EACH_PROCESS(p) {
-		if(p->state && p->state != PROC_ZOMBIE) {
+		if(p->state != PROC_ZOMBIE) {
 			printk("%d    %5d  %5d  %s  %s\n",
 				p->uid,
 				p->pid,
@@ -38,6 +38,7 @@ static void process_list(void)
 				p->argv0
 			);
 		}
+		p = p->next;
 	}
 }
 

drivers/char/tty.c
@@ -245,9 +245,10 @@ void disassociate_ctty(struct tty *tty)
 
 	/* clear the controlling tty for all processes in the same SID */
 	FOR_EACH_PROCESS(p) {
-		if((p->state != PROC_UNUSED) && (p->sid == current->sid)) {
+		if(p->sid == current->sid) {
 			p->ctty = NULL;
 		}
+		p = p->next;
 	}
 	kill_pgrp(current->pgid, SIGHUP);
 	kill_pgrp(current->pgid, SIGCONT);
@@ -478,9 +479,10 @@ int tty_close(struct inode *i, struct fd *fd_table)
 
 		/* this tty is no longer the controlling tty of any process */
 		FOR_EACH_PROCESS(p) {
-			if((p->state != PROC_UNUSED) && (p->ctty == tty)) {
+			if(p->ctty == tty) {
 				p->ctty = NULL;
 			}
+			p = p->next;
 		}
 	}
 	return 0;
@@ -840,9 +842,10 @@ int tty_ioctl(struct inode *i, int cmd, unsigned long int arg)
 			if(tty->sid) {
 				if((arg == 1) && IS_SUPERUSER) {
 					FOR_EACH_PROCESS(p) {
-						if((p->state != PROC_UNUSED) && (p->ctty == tty)) {
+						if(p->ctty == tty) {
 							p->ctty = NULL;
 						}
+						p = p->next;
 					}
 				} else {
 					return -EPERM;

fs/procfs/data.c
@@ -184,12 +184,11 @@ int data_proc_loadavg(char *buffer, __pid_t pid)
 	c = avenrun[2] << (SI_LOAD_SHIFT - FSHIFT);
 
 	FOR_EACH_PROCESS(p) {
-		if(p->state) {
-			nprocs++;
-			if(p->state == PROC_RUNNING) {
-				nrun++;
-			}
+		nprocs++;
+		if(p->state == PROC_RUNNING) {
+			nrun++;
 		}
+		p = p->next;
 	}
 
 	size = sprintk(buffer, "%d.%02d %d.%02d %d.%02d %d/%d %d\n", LOAD_INT(a), LOAD_FRAC(a), LOAD_INT(b), LOAD_FRAC(b), LOAD_INT(c), LOAD_FRAC(c), nrun, nprocs, lastpid);

fs/procfs/dir.c
@@ -70,29 +70,28 @@ static int proc_listdir(char *buffer, int count)
 	pd = (struct procfs_dir_entry *)buffer;
 
 	FOR_EACH_PROCESS(p) {
-		if(p->state) {
-			d.inode = PROC_PID_INO + (p->pid << 12);
-			d.mode = S_IFDIR | S_IRUSR | S_IXUSR | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH;
-			d.nlink = 1;
-			d.lev = -1;
-			d.name_len = 1;
-			n = p->pid;
-			while(n / 10) {
-				n /= 10;
-				d.name_len++;
-			}
-			d.name = p->pidstr;
-			d.data_fn = NULL;
-
-			if(size + sizeof(struct procfs_dir_entry) > (count - 1)) {
-				printk("WARNING: kmalloc() is limited to 4096 bytes.\n");
-				break;
-			}
-
-			size += sizeof(struct procfs_dir_entry);
-			memcpy_b((void *)pd, (void *)&d, sizeof(struct procfs_dir_entry));
-			pd++;
+		d.inode = PROC_PID_INO + (p->pid << 12);
+		d.mode = S_IFDIR | S_IRUSR | S_IXUSR | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH;
+		d.nlink = 1;
+		d.lev = -1;
+		d.name_len = 1;
+		n = p->pid;
+		while(n / 10) {
+			n /= 10;
+			d.name_len++;
 		}
+		d.name = p->pidstr;
+		d.data_fn = NULL;
+
+		if(size + sizeof(struct procfs_dir_entry) > (count - 1)) {
+			printk("WARNING: kmalloc() is limited to 4096 bytes.\n");
+			break;
+		}
+
+		size += sizeof(struct procfs_dir_entry);
+		memcpy_b((void *)pd, (void *)&d, sizeof(struct procfs_dir_entry));
+		pd++;
+		p = p->next;
 	}
 	return size;
 }

fs/procfs/namei.c
@@ -68,20 +68,19 @@ int procfs_lookup(const char *name, struct inode *dir, struct inode **i_res)
 	}
 
 	FOR_EACH_PROCESS(p) {
-		if(p->state) {
-			if(len == strlen(p->pidstr)) {
-				if(!(strcmp(p->pidstr, name))) {
-					inode = PROC_PID_INO + (p->pid << 12);
-				}
-			}
-			if(inode) {
-				if(!(*i_res = iget(dir->sb, inode))) {
-					return -EACCES;
-				}
-				iput(dir);
-				return 0;
+		if(len == strlen(p->pidstr)) {
+			if(!(strcmp(p->pidstr, name))) {
+				inode = PROC_PID_INO + (p->pid << 12);
 			}
 		}
+		if(inode) {
+			if(!(*i_res = iget(dir->sb, inode))) {
+				return -EACCES;
+			}
+			iput(dir);
+			return 0;
+		}
+		p = p->next;
 	}
 	iput(dir);
 	return -ENOENT;

include/fiwix/process.h
@@ -47,7 +47,7 @@ struct vma {
 #define PG_LEADER(p)	((p)->pid == (p)->pgid)
 #define SESS_LEADER(p)	((p)->pid == (p)->pgid && (p)->pid == (p)->sid)
 
-#define FOR_EACH_PROCESS(p)	for(p = &proc_table[INIT]; p; p = p->next)
+#define FOR_EACH_PROCESS(p)		p = proc_table_head->next ; while(p)
 #define FOR_EACH_PROCESS_RUNNING(p)	p = run_head ; while(p)
 
 /* value to be determined during system startup */
@@ -56,6 +56,7 @@ extern unsigned int proc_table_size;	/* size in bytes */
 extern char any_key_to_reboot;
 extern int nr_processes;
 extern __pid_t lastpid;
+extern struct proc *proc_table_head;
 
 struct binargs {
 	unsigned int page[ARG_MAX];

include/fiwix/sched.h
@@ -14,7 +14,6 @@
 #define PRIO_PGRP	1
 #define PRIO_USER	2
 
-#define PROC_UNUSED	0
 #define PROC_RUNNING	1
 #define PROC_SLEEPING	2
 #define PROC_ZOMBIE	3

commit 42fab70684c181be87affb3020cab1aeb15c11d6

fixed a bug in syscalls/wait4.c introduced in 9cfe372 that created malfunction in control jobs

kernel/syscalls/wait4.c
@@ -59,12 +59,14 @@ int sys_wait4(__pid_t pid, int *status, int options, struct rusage *ru)
 			}
 			if(flag) {
 				if(p->state == PROC_STOPPED) {
-					if(p->exit_code) {
-						if(status) {
-							*status = (p->exit_code << 8) | 0x7F;
-						}
-						p->exit_code = 0;
+					if(!p->exit_code) {
+						p = p->next;
+						continue;
 					}
+					if(status) {
+						*status = (p->exit_code << 8) | 0x7F;
+					}
+					p->exit_code = 0;
 					if(ru) {
 						get_rusage(p, ru);
 					}