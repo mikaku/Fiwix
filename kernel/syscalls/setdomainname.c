01cf4f147b6d4a52b135747a5dffe910af15b6fb
Jordi Sanfeliu <jordi@fibranet.cat>
2023-02-20 16:22:23 +0100

    fixed sys_setdomainname() to use malloc_name() and free_name() to validate the argument 'name'

--- kernel/syscalls/setdomainname.c
+++ kernel/syscalls/setdomainname.c
@@ -18,21 +18,25 @@
 int sys_setdomainname(const char *name, int length)
 {
 	int errno;
+	char *tmp_name;
 
 #ifdef __DEBUG__
 	printk("(pid %d) sys_setdomainname('%s', %d)\n", current->pid, name, length);
 #endif /*__DEBUG__ */
 
-	if((errno = check_user_area(VERIFY_READ, name, length))) {
+	if((errno = malloc_name(name, &tmp_name)) < 0) {
 		return errno;
 	}
 	if(!IS_SUPERUSER) {
+		free_name(tmp_name);
 		return -EPERM;
 	}
 	if(length < 0 || length > _UTSNAME_LENGTH) {
+		free_name(tmp_name);
 		return -EINVAL;
 	}
-	memcpy_b(&sys_utsname.domainname, name, length);
+	memcpy_b(&sys_utsname.domainname, tmp_name, length);
 	sys_utsname.domainname[length] = 0;
+	free_name(tmp_name);
 	return 0;
 }
