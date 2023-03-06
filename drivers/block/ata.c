mikaku/Fiwix

commit b985ec132675be10f01dc415c00faddc3cdb2714

Major rewrite of the ATA disk driver (also ATAPI)
This new driver supports 32bit transfers where available, improving the
I/O throughput with a 30% faster.

These changes should be part for upcomming DMA support.

drivers/block/ata_hd.c
@@ -1,0 +1,410 @@
+/*
+ * fiwix/drivers/block/ata_hd.c
+ *
+ * Copyright 2018-2022, Jordi Sanfeliu. All rights reserved.
+ * Distributed under the terms of the Fiwix License.
+ */
+
+#include <fiwix/asm.h>
+#include <fiwix/buffer.h>
+#include <fiwix/ata.h>
+#include <fiwix/ata_hd.h>
+#include <fiwix/ioctl.h>
+#include <fiwix/devices.h>
+#include <fiwix/timer.h>
+#include <fiwix/cpu.h>
+#include <fiwix/part.h>
+#include <fiwix/mm.h>
+#include <fiwix/errno.h>
+#include <fiwix/stdio.h>
+#include <fiwix/string.h>
+
+static struct fs_operations ata_hd_driver_fsop = {
+	0,
+	0,
+
+	ata_hd_open,
+	ata_hd_close,
+	NULL,			/* read */
+	NULL,			/* write */
+	ata_hd_ioctl,
+	NULL,			/* lseek */
+	NULL,			/* readdir */
+	NULL,			/* mmap */
+	NULL,			/* select */
+
+	NULL,			/* readlink */
+	NULL,			/* followlink */
+	NULL,			/* bmap */
+	NULL,			/* lockup */
+	NULL,			/* rmdir */
+	NULL,			/* link */
+	NULL,			/* unlink */
+	NULL,			/* symlink */
+	NULL,			/* mkdir */
+	NULL,			/* mknod */
+	NULL,			/* truncate */
+	NULL,			/* create */
+	NULL,			/* rename */
+
+	ata_hd_read,
+	ata_hd_write,
+
+	NULL,			/* read_inode */
+	NULL,			/* write_inode */
+	NULL,			/* ialloc */
+	NULL,			/* ifree */
+	NULL,			/* statfs */
+	NULL,			/* read_superblock */
+	NULL,			/* remount_fs */
+	NULL,			/* write_superblock */
+	NULL			/* release_superblock */
+};
+
+static void assign_minors(__dev_t rdev, struct ata_drv *drive, struct partition *part)
+{
+	int n, minor;
+	struct device *d;
+
+	minor = 0;
+
+	if(!(d = get_device(BLK_DEV, rdev))) {
+		printk("WARNING: %s(): unable to assign minors to device %d,%d.\n", __FUNCTION__, MAJOR(rdev), MINOR(rdev));
+		return;
+	}
+
+	for(n = 0; n < NR_PARTITIONS; n++) {
+		if(drive->num == IDE_MASTER) {
+			minor = (1 << drive->minor_shift) + n;
+		}
+		if(drive->num == IDE_SLAVE) {
+			minor = (1 << drive->minor_shift) + n + 1;
+		}
+		CLEAR_MINOR(d->minors, minor);
+		if(part[n].type) {
+			SET_MINOR(d->minors, minor);
+			((unsigned int *)d->device_data)[minor] = part[n].nr_sects / 2;
+		}
+	}
+}
+
+static __off_t block2sector(__blk_t block, int blksize, struct partition *part, int minor)
+{
+	__off_t sector;
+
+	sector = block * (blksize / ATA_HD_SECTSIZE);
+	if(minor) {
+		sector += part[minor - 1].startsect;
+	}
+	return sector;
+}
+
+int ata_hd_open(struct inode *i, struct fd *fd_table)
+{
+	return 0;
+}
+
+int ata_hd_close(struct inode *i, struct fd *fd_table)
+{
+	sync_buffers(i->rdev);
+	return 0;
+}
+
+int ata_hd_read(__dev_t dev, __blk_t block, char *buffer, int blksize)
+{
+	int minor;
+	int sectors_to_read, datalen;
+	int n, status, r, retries;
+	__off_t offset;
+	struct ide *ide;
+	struct ata_drv *drive;
+	struct partition *part;
+
+	if(!(ide = get_ide_controller(dev))) {
+		return -EINVAL;
+	}
+
+	minor = MINOR(dev);
+	drive = &ide->drive[GET_DRIVE_NUM(dev)];
+	if(drive->num) {
+		minor &= ~(1 << IDE_SLAVE_MSF);
+	}
+
+	SET_ATA_RDY_RETR(retries);
+
+	blksize = blksize ? blksize : BLKSIZE_1K;
+	sectors_to_read = MIN(blksize, PAGE_SIZE) / ATA_HD_SECTSIZE;
+
+	part = drive->part_table;
+	offset = block2sector(block, blksize, part, minor);
+
+	if(drive->flags & DRIVE_HAS_RW_MULTIPLE) {
+		datalen = (ATA_HD_SECTSIZE * sectors_to_read) / drive->xfer.copy_raw_factor;
+	} else {
+		datalen = ATA_HD_SECTSIZE / drive->xfer.copy_raw_factor;
+	}
+
+	CLI();
+	lock_resource(&ide->resource);
+
+	for(n = 0; n < sectors_to_read; n++) {
+		if(ata_io(ide, drive, offset)) {
+			unlock_resource(&ide->resource);
+			return -EIO;
+		}
+		if(ata_wait_irq(ide, WAIT_FOR_DISK, drive->xfer.read_cmd)) {
+			printk("WARNING: %s(): %s: timeout on hard disk dev %d,%d during read.\n", __FUNCTION__, drive->dev_name, MAJOR(dev), MINOR(dev));
+			unlock_resource(&ide->resource);
+			return -EIO;
+		}
+
+		for(r = 0; r < retries; r++) {
+			status = inport_b(ide->base + ATA_STATUS);
+			if(!(status & ATA_STAT_BSY) && (status & ATA_STAT_DRQ)) {
+				break;
+			}
+			ata_delay();
+		}
+		if(status & ATA_STAT_ERR) {
+			printk("WARNING: %s(): %s: error on hard disk dev %d,%d during read.\n", __FUNCTION__, drive->dev_name, MAJOR(dev), MINOR(dev));
+			printk("\tstatus=0x%x ", status);
+			ata_error(ide, status);
+			printk("\tblock %d, sector %d.\n", block, offset);
+			inport_b(ide->base + ATA_STATUS);	/* clear any pending interrupt */
+			unlock_resource(&ide->resource);
+			return -EIO;
+		}
+
+		drive->xfer.read_fn(ide->base + ATA_DATA, (void *)buffer, datalen);
+		if(drive->flags & DRIVE_HAS_RW_MULTIPLE) {
+			break;
+		}
+		offset++;
+		buffer += ATA_HD_SECTSIZE;
+	}
+	inport_b(ide->base + ATA_STATUS);	/* clear any pending interrupt */
+	unlock_resource(&ide->resource);
+	return sectors_to_read * ATA_HD_SECTSIZE;
+}
+
+int ata_hd_write(__dev_t dev, __blk_t block, char *buffer, int blksize)
+{
+	int minor;
+	int sectors_to_write, datalen;
+	int n, status, r, retries;
+	__off_t offset;
+	struct ide *ide;
+	struct ata_drv *drive;
+	struct partition *part;
+
+	if(!(ide = get_ide_controller(dev))) {
+		return -EINVAL;
+	}
+
+	minor = MINOR(dev);
+	drive = &ide->drive[GET_DRIVE_NUM(dev)];
+	if(drive->num) {
+		minor &= ~(1 << IDE_SLAVE_MSF);
+	}
+
+	SET_ATA_RDY_RETR(retries);
+
+	blksize = blksize ? blksize : BLKSIZE_1K;
+	sectors_to_write = MIN(blksize, PAGE_SIZE) / ATA_HD_SECTSIZE;
+
+	part = drive->part_table;
+	offset = block2sector(block, blksize, part, minor);
+
+	if(drive->flags & DRIVE_HAS_RW_MULTIPLE) {
+		datalen = (ATA_HD_SECTSIZE * sectors_to_write) / drive->xfer.copy_raw_factor;
+	} else {
+		datalen = ATA_HD_SECTSIZE / drive->xfer.copy_raw_factor;
+	}
+
+	CLI();
+	lock_resource(&ide->resource);
+
+	for(n = 0; n < sectors_to_write; n++) {
+		if(ata_io(ide, drive, offset)) {
+			unlock_resource(&ide->resource);
+			return -EIO;
+		}
+		outport_b(ide->base + ATA_COMMAND, drive->xfer.write_cmd);
+
+		for(r = 0; r < retries; r++) {
+			status = inport_b(ide->base + ATA_STATUS);
+			if(!(status & ATA_STAT_BSY) && (status & ATA_STAT_DRQ)) {
+				break;
+			}
+			ata_delay();
+		}
+		if(status & ATA_STAT_ERR) {
+			printk("WARNING: %s(): %s: error on hard disk dev %d,%d during write.\n", __FUNCTION__, drive->dev_name, MAJOR(dev), MINOR(dev));
+			printk("\tstatus=0x%x ", status);
+			ata_error(ide, status);
+			printk("\tblock %d, sector %d.\n", block, offset);
+			inport_b(ide->base + ATA_STATUS);	/* clear any pending interrupt */
+			unlock_resource(&ide->resource);
+			return -EIO;
+		}
+
+		drive->xfer.write_fn(ide->base + ATA_DATA, (void *)buffer, datalen);
+		if(ata_wait_irq(ide, WAIT_FOR_DISK, 0)) {
+			printk("WARNING: %s(): %s: timeout on hard disk dev %d,%d during read.\n", __FUNCTION__, drive->dev_name, MAJOR(dev), MINOR(dev));
+			unlock_resource(&ide->resource);
+			return -EIO;
+		}
+
+		if(drive->flags & DRIVE_HAS_RW_MULTIPLE) {
+			break;
+		}
+		offset++;
+		buffer += ATA_HD_SECTSIZE;
+	}
+	inport_b(ide->base + ATA_STATUS);	/* clear any pending interrupt */
+	unlock_resource(&ide->resource);
+	return sectors_to_write * ATA_HD_SECTSIZE;
+}
+
+int ata_hd_ioctl(struct inode *i, int cmd, unsigned long int arg)
+{
+	int minor;
+	struct ide *ide;
+	struct ata_drv *drive;
+	struct partition *part;
+	struct hd_geometry *geom;
+	int errno;
+
+	if(!(ide = get_ide_controller(i->rdev))) {
+		return -EINVAL;
+	}
+
+	minor = MINOR(i->rdev);
+	drive = &ide->drive[GET_DRIVE_NUM(i->rdev)];
+	if(drive->num) {
+		minor &= ~(1 << IDE_SLAVE_MSF);
+	}
+
+	part = drive->part_table;
+
+	switch(cmd) {
+		case HDIO_GETGEO:
+			if((errno = check_user_area(VERIFY_WRITE, (void *)arg, sizeof(struct hd_geometry)))) {
+				return errno;
+			}
+			geom = (struct hd_geometry *)arg;
+	                geom->cylinders = drive->ident.logic_cyls;
+	                geom->heads = (char)drive->ident.logic_heads;
+	                geom->sectors = (char)drive->ident.logic_spt;
+			geom->start = 0;
+			if(minor) {
+	                	geom->start = part[minor - 1].startsect;
+			}
+			break;
+		case BLKGETSIZE:
+			if((errno = check_user_area(VERIFY_WRITE, (void *)arg, sizeof(unsigned int)))) {
+				return errno;
+			}
+			if(!minor) {
+				*(int *)arg = (unsigned int)drive->nr_sects;
+			} else {
+				*(int *)arg = (unsigned int)drive->part_table[minor - 1].nr_sects;
+			}
+			break;
+		case BLKFLSBUF:
+			sync_buffers(i->rdev);
+			invalidate_buffers(i->rdev);
+			break;
+		case BLKRRPART:
+			read_msdos_partition(i->rdev, part);
+			assign_minors(i->rdev, drive, part);
+			break;
+		default:
+			return -EINVAL;
+			break;
+	}
+
+	return 0;
+}
+
+int ata_hd_init(struct ide *ide, struct ata_drv *drive)
+{
+	int n, status;
+	__dev_t rdev;
+	struct device *d;
+	struct partition *part;
+
+	rdev = 0;
+	drive->fsop = &ata_hd_driver_fsop;
+	part = drive->part_table;
+
+	if(drive->num == IDE_MASTER) {
+		rdev = MKDEV(drive->major, drive->num);
+		drive->minor_shift = IDE_MASTER_MSF;
+		if(!(d = get_device(BLK_DEV, rdev))) {
+			return -EINVAL;
+		}
+		((unsigned int *)d->device_data)[0] = drive->nr_sects / 2;
+	} else {
+		rdev = MKDEV(drive->major, 1 << IDE_SLAVE_MSF);
+		drive->minor_shift = IDE_SLAVE_MSF;
+		if(!(d = get_device(BLK_DEV, rdev))) {
+			return -EINVAL;
+		}
+		((unsigned int *)d->device_data)[1 << IDE_SLAVE_MSF] = drive->nr_sects / 2;
+	}
+
+	outport_b(ide->ctrl + ATA_DEV_CTRL, ATA_DEVCTR_NIEN);
+	if(drive->flags & DRIVE_HAS_RW_MULTIPLE) {
+		/* read/write in 4KB blocks (8 sectors) by default */
+		outport_b(ide->base + ATA_SECCNT, PAGE_SIZE / ATA_HD_SECTSIZE);
+		outport_b(ide->base + ATA_COMMAND, ATA_SET_MULTIPLE_MODE);
+		ata_wait400ns(ide);
+		while(inport_b(ide->base + ATA_STATUS) & ATA_STAT_BSY);
+	}
+	outport_b(ide->ctrl + ATA_DEV_CTRL, ATA_DEVCTR_DRQ);
+
+	/* setup the transfer mode */
+	for(;;) {
+		status = inport_b(ide->base + ATA_STATUS);
+		if(!(status & (ATA_STAT_BSY | ATA_STAT_DRQ))) {
+			break;
+		}
+		ata_delay();
+	}
+	if(ata_select_drv(ide, drive->num, 0, 0)) {
+		printk("WARNING: %s(): %s: drive not ready.\n", __FUNCTION__, drive->dev_name);
+	}
+	outport_b(ide->base + ATA_FEATURES, ATA_SET_XFERMODE);
+	outport_b(ide->base + ATA_SECCNT, 0x08 | 4);	/* FIXME drive->piomode, ... */
+	outport_b(ide->base + ATA_SECTOR, 0);
+	outport_b(ide->base + ATA_LCYL, 0);
+	outport_b(ide->base + ATA_HCYL, 0);
+	ata_wait_irq(ide, WAIT_FOR_DISK, ATA_SET_FEATURES);
+	while(!(inport_b(ide->base + ATA_STATUS) & ATA_STAT_RDY));
+	ata_wait400ns(ide);
+	status = inport_b(ide->base + ATA_STATUS);
+	if(status & (ATA_STAT_ERR | ATA_STAT_DWF)) {
+		printk("WARNING: %s(): error while setting transfer mode.\n", __FUNCTION__);
+		printk("\t");
+		ata_error(ide, status);
+		printk("\n");
+	}
+
+	/* show disk partition summary */
+	read_msdos_partition(rdev, part);
+	assign_minors(rdev, drive, part);
+	printk("\t\t\t\tpartition summary: ");
+	for(n = 0; n < NR_PARTITIONS; n++) {
+		/* status values other than 0x00 and 0x80 are invalid */
+		if(part[n].status && part[n].status != 0x80) {
+			continue;
+		}
+		if(part[n].type) {
+			printk("%s%d ", drive->dev_name, n + 1);
+		}
+	}
+	printk("\n");
+
+	return 0;
+}

drivers/block/Makefile
@@ -9,7 +9,7 @@
 .c.o:
 	$(CC) $(CFLAGS) -c -o $@ $<
 
-OBJS = dma.o floppy.o part.o ide.o ide_hd.o ide_cd.o ramdisk.o
+OBJS = dma.o floppy.o part.o ata.o ata_hd.o atapi.o atapi_cd.o ramdisk.o
 
 all:	$(OBJS)
 

drivers/block/atapi.c
@@ -1,0 +1,205 @@
+/*
+ * fiwix/drivers/block/atapi.c
+ *
+ * Copyright 2023, Jordi Sanfeliu. All rights reserved.
+ * Distributed under the terms of the Fiwix License.
+ */
+
+#include <fiwix/asm.h>
+#include <fiwix/ata.h>
+#include <fiwix/atapi.h>
+#include <fiwix/atapi_cd.h>
+#include <fiwix/timer.h>
+#include <fiwix/cpu.h>
+#include <fiwix/errno.h>
+#include <fiwix/stdio.h>
+#include <fiwix/string.h>
+
+int send_packet_command(unsigned char *pkt, struct ide *ide, struct ata_drv *drive, int blksize)
+{
+	int n, retries, status;
+
+	outport_b(ide->ctrl + ATA_DEV_CTRL, 0);
+	ata_delay();
+	outport_b(ide->base + ATA_DRVHD, ATA_CHS_MODE);
+	ata_delay();
+	if(ata_select_drv(ide, drive->num, ATA_CHS_MODE, 0)) {
+		printk("WARNING: %s(): %s: drive not ready to receive PACKET command.\n", __FUNCTION__, drive->dev_name);
+		return 1;
+	}
+
+	CLI();
+	outport_b(ide->base + ATA_FEATURES, 0);
+	outport_b(ide->base + ATA_SECCNT, 0);
+	outport_b(ide->base + ATA_SECTOR, 0);
+	outport_b(ide->base + ATA_LCYL, blksize & 0xFF);
+	outport_b(ide->base + ATA_HCYL, blksize >> 8);
+	outport_b(ide->base + ATA_DRVHD, drive->num << 4);
+	outport_b(ide->base + ATA_COMMAND, ATA_PACKET);
+	ata_wait400ns(ide);
+
+	/*
+	 * NOTE: Some devices prior to ATA/ATAPI-4 assert INTRQ if enabled
+	 * at this point. See IDENTIFY PACKET DEVICE, word 0, bits 5-6 to
+	 * determine if an interrupt will occur.
+	 */
+
+	SET_ATA_RDY_RETR(retries);
+
+	for(n = 0; n < retries; n++) {
+		status = inport_b(ide->base + ATA_STATUS);
+		if((status & (ATA_STAT_DRQ | ATA_STAT_BSY)) == ATA_STAT_DRQ) {
+			break;
+		}
+		ata_delay();
+	}
+	if(n >= retries) {
+		printk("WARNING: %s(): %s: drive not ready to receive command packet (retries = %d).\n", __FUNCTION__, drive->dev_name, n);
+		return 1;
+	}
+
+	outport_sw(ide->base + ATA_DATA, pkt, 12 / sizeof(short int));
+	return 0;
+}
+
+int atapi_read_data(__dev_t dev, char *data, struct ide *ide, struct ata_drv *drive, int blksize, int offset)
+{
+	int errno, status;
+	char *buffer;
+	int retries, bytes;
+
+	status = 0;
+
+	for(retries = 0; retries < MAX_IDE_ERR; retries++) {
+		if(ata_wait_irq(ide, WAIT_FOR_CD, drive->xfer.read_cmd)) {
+			continue;
+		}
+		status = inport_b(ide->base + ATA_STATUS);
+		if(status & ATA_STAT_ERR) {
+			continue;
+		}
+
+		if((status & (ATA_STAT_DRQ | ATA_STAT_BSY)) == 0) {
+			break;
+		}
+
+		bytes = (inport_b(ide->base + ATA_HCYL) << 8) + inport_b(ide->base + ATA_LCYL);
+		if(!bytes || bytes > blksize) {
+			break;
+		}
+
+		bytes = MAX(bytes, ATA_CD_SECTSIZE);	/* read more than 2048 bytes is not supported */
+		buffer = data + offset;
+		drive->xfer.read_fn(ide->base + ATA_DATA, (void *)buffer, bytes / drive->xfer.copy_raw_factor);
+	}
+
+	if(status & ATA_STAT_ERR) {
+		errno = inport_b(ide->base + ATA_ERROR);
+		printk("WARNING: %s(): error on cdrom device %d,%d, status=0x%x error=0x%x,\n", __FUNCTION__, MAJOR(dev), MINOR(dev), status, errno);
+		return 1;
+	}
+
+	if(retries >= MAX_IDE_ERR) {
+		printk("WARNING: %s(): timeout on cdrom device %d,%d, status=0x%x.\n", __FUNCTION__, MAJOR(dev), MINOR(dev), status);
+		/* a reset may be required at this moment */
+		return 1;
+	}
+	return 0;
+}
+
+int atapi_cmd_testunit(struct ide *ide, struct ata_drv *drive)
+{
+	unsigned char pkt[12];
+
+	pkt[0] = ATAPI_TEST_UNIT;
+	pkt[1] = 0;
+	pkt[2] = 0;
+	pkt[3] = 0;
+	pkt[4] = 0;
+	pkt[5] = 0;
+	pkt[6] = 0;
+	pkt[7] = 0;
+	pkt[8] = 0;
+	pkt[9] = 0;
+	pkt[10] = 0;
+	pkt[11] = 0;
+	return send_packet_command(pkt, ide, drive, 0);
+}
+
+int atapi_cmd_reqsense(struct ide *ide, struct ata_drv *drive)
+{
+	unsigned char pkt[12];
+
+	pkt[0] = ATAPI_REQUEST_SENSE;
+	pkt[1] = 0;
+	pkt[2] = 0;
+	pkt[3] = 0;
+	pkt[4] = 252;	/* this command can send up to 252 bytes */
+	pkt[5] = 0;
+	pkt[6] = 0;
+	pkt[7] = 0;
+	pkt[8] = 0;
+	pkt[9] = 0;
+	pkt[10] = 0;
+	pkt[11] = 0;
+	return send_packet_command(pkt, ide, drive, 0);
+}
+
+int atapi_cmd_startstop(int action, struct ide *ide, struct ata_drv *drive)
+{
+	unsigned char pkt[12];
+
+	pkt[0] = ATAPI_START_STOP;
+	pkt[1] = 0;
+	pkt[2] = 0;
+	pkt[3] = 0;
+	pkt[4] = action;
+	pkt[5] = 0;
+	pkt[6] = 0;
+	pkt[7] = 0;
+	pkt[8] = 0;
+	pkt[9] = 0;
+	pkt[10] = 0;
+	pkt[11] = 0;
+	return send_packet_command(pkt, ide, drive, 0);
+}
+
+int atapi_cmd_mediumrm(int action, struct ide *ide, struct ata_drv *drive)
+{
+	unsigned char pkt[12];
+
+	pkt[0] = ATAPI_MEDIUM_REMOVAL;
+	pkt[1] = 0;
+	pkt[2] = 0;
+	pkt[3] = 0;
+	pkt[4] = action;
+	pkt[5] = 0;
+	pkt[6] = 0;
+	pkt[7] = 0;
+	pkt[8] = 0;
+	pkt[9] = 0;
+	pkt[10] = 0;
+	pkt[11] = 0;
+	return send_packet_command(pkt, ide, drive, 0);
+}
+
+int request_sense(char *buffer, __dev_t dev, struct ide *ide, struct ata_drv *drive)
+{
+	int errcode;
+	int sense_key, sense_asc;
+
+	errcode = inport_b(ide->base + ATA_ERROR);
+	sense_key = (errcode & 0xF0) >> 4;
+	printk("\tSense Key code indicates a '%s' condition.\n", sense_key_err[sense_key & 0xF]);
+	errcode = atapi_cmd_reqsense(ide, drive);
+	printk("reqsense() returned %d\n", errcode);
+	errcode = atapi_read_data(dev, buffer, ide, drive, BLKSIZE_2K, 0);
+	printk("atapi_read_data() returned %d\n", errcode);
+	errcode = (int)(buffer[0] & 0x7F);
+	sense_key = (int)(buffer[2] & 0xF);
+	sense_asc = (int)(buffer[12] & 0xFF);
+	printk("errcode = %x\n", errcode);
+	printk("sense_key = %x\n", sense_key);
+	printk("sense_asc = %x\n", sense_asc);
+	return errcode;
+}

drivers/block/atapi_cd.c
@@ -1,0 +1,257 @@
+/*
+ * fiwix/drivers/block/atapi_cd.c
+ *
+ * Copyright 2018-2022, Jordi Sanfeliu. All rights reserved.
+ * Distributed under the terms of the Fiwix License.
+ */
+
+#include <fiwix/asm.h>
+#include <fiwix/buffer.h>
+#include <fiwix/atapi.h>
+#include <fiwix/atapi_cd.h>
+#include <fiwix/devices.h>
+#include <fiwix/mm.h>
+#include <fiwix/errno.h>
+#include <fiwix/stdio.h>
+#include <fiwix/string.h>
+
+/* default size of 1GB is enough to read a whole CDROM */
+#define CDROM_DEFAULT_SIZE	(1024 * 1024)	/* in KBs */
+
+static struct fs_operations ata_cd_driver_fsop = {
+	0,
+	0,
+
+	ata_cd_open,
+	ata_cd_close,
+	NULL,			/* read */
+	NULL,			/* write */
+	ata_cd_ioctl,
+	NULL,			/* lseek */
+	NULL,			/* readdir */
+	NULL,			/* mmap */
+	NULL,			/* select */
+
+	NULL,			/* readlink */
+	NULL,			/* followlink */
+	NULL,			/* bmap */
+	NULL,			/* lockup */
+	NULL,			/* rmdir */
+	NULL,			/* link */
+	NULL,			/* unlink */
+	NULL,			/* symlink */
+	NULL,			/* mkdir */
+	NULL,			/* mknod */
+	NULL,			/* truncate */
+	NULL,			/* create */
+	NULL,			/* rename */
+
+	ata_cd_read,
+	NULL,			/* write_block */
+
+	NULL,			/* read_inode */
+	NULL,			/* write_inode */
+	NULL,			/* ialloc */
+	NULL,			/* ifree */
+	NULL,			/* statfs */
+	NULL,			/* read_superblock */
+	NULL,			/* remount_fs */
+	NULL,			/* write_superblock */
+	NULL			/* release_superblock */
+};
+
+int ata_cd_open(struct inode *i, struct fd *fd_table)
+{
+	char *buffer;
+	int errcode;
+	int sense_key, sense_asc;
+	int retries;
+	struct ide *ide;
+	struct ata_drv *drive;
+
+	if(!(ide = get_ide_controller(i->rdev))) {
+		return -EINVAL;
+	}
+
+	drive = &ide->drive[GET_DRIVE_NUM(i->rdev)];
+
+	lock_resource(&ide->resource);
+
+	if(!(buffer = (void *)kmalloc())) {
+		unlock_resource(&ide->resource);
+		return -ENOMEM;
+	}
+
+	if((errcode = atapi_cmd_testunit(ide, drive))) {
+		printk("WARNING: %s(): cdrom device %d,%d is not ready for TEST_UNIT, error %d.\n", __FUNCTION__, MAJOR(i->rdev), MINOR(i->rdev), errcode);
+		request_sense(buffer, i->rdev, ide, drive);
+	}
+
+	for(retries = 0; retries < MAX_CD_ERR; retries++) {
+		if(!(errcode = atapi_cmd_startstop(CD_LOAD, ide, drive))) {
+			break;
+		}
+		printk("WARNING: %s(): cdrom device %d,%d is not ready for CD_LOAD, error %d.\n", __FUNCTION__, MAJOR(i->rdev), MINOR(i->rdev), errcode);
+		atapi_read_data(i->rdev, buffer, ide, drive, BLKSIZE_2K, 0);
+		errcode = request_sense(buffer, i->rdev, ide, drive);
+		sense_key = (errcode & 0xF0) >> 4;
+		/* trying to eject on slim drives may lead to an illegal request */
+		if(!sense_key || sense_key == RS_ILLEGAL_REQUEST) {
+			break;
+		}
+		if(errcode == 0x70 || errcode == 0x71) {
+			sense_key = (int)(buffer[2] & 0xF);
+			sense_asc = (int)(buffer[12] & 0xFF);
+			if(sense_key == RS_NOT_READY && sense_asc == ASC_NO_MEDIUM) {
+				kfree((unsigned int)buffer);
+				unlock_resource(&ide->resource);
+				return -ENOMEDIUM;
+			}
+		}
+	}
+
+	if(retries == MAX_CD_ERR) {
+		if(sense_key == RS_NOT_READY) {
+			kfree((unsigned int)buffer);
+			unlock_resource(&ide->resource);
+			return -ENOMEDIUM;
+		}
+	}
+
+	if(atapi_cmd_mediumrm(CD_LOCK_MEDIUM, ide, drive)) {
+		printk("WARNING: %s(): error on cdrom device %d,%d while trying to lock medium.\n", __FUNCTION__, MAJOR(i->rdev), MINOR(i->rdev), ATAPI_MEDIUM_REMOVAL);
+		request_sense(buffer, i->rdev, ide, drive);
+	}
+
+	/* this line just to catch interrupt */
+	atapi_read_data(i->rdev, buffer, ide, drive, BLKSIZE_2K, 0);
+	kfree((unsigned int)buffer);
+
+	unlock_resource(&ide->resource);
+	return 0;
+}
+
+int ata_cd_close(struct inode *i, struct fd *fd_table)
+{
+	char *buffer;
+	struct ide *ide;
+	struct ata_drv *drive;
+
+	if(!(ide = get_ide_controller(i->rdev))) {
+		return -EINVAL;
+	}
+
+	if(!(buffer = (void *)kmalloc())) {
+		return -ENOMEM;
+	}
+
+	drive = &ide->drive[GET_DRIVE_NUM(i->rdev)];
+
+	/* FIXME: only if device usage == 0 */
+	invalidate_buffers(i->rdev);
+
+	if(atapi_cmd_mediumrm(CD_UNLOCK_MEDIUM, ide, drive)) {
+		printk("WARNING: %s(): error on cdrom device %d,%d during 0x%x command.\n", __FUNCTION__, MAJOR(i->rdev), MINOR(i->rdev), ATAPI_MEDIUM_REMOVAL);
+	}
+
+	/* this line just to catch interrupt */
+	atapi_read_data(i->rdev, buffer, ide, drive, BLKSIZE_2K, 0);
+	kfree((unsigned int)buffer);
+
+	return 0;
+}
+
+int ata_cd_read(__dev_t dev, __blk_t block, char *buffer, int blksize)
+{
+	int sectors_to_read;
+	int n, retries;
+	unsigned char pkt[12];
+	struct ide *ide;
+	struct ata_drv *drive;
+
+	if(!(ide = get_ide_controller(dev))) {
+		return -EINVAL;
+	}
+
+	drive = &ide->drive[GET_DRIVE_NUM(dev)];
+	blksize = BLKSIZE_2K;
+	sectors_to_read = blksize / ATA_CD_SECTSIZE;
+
+	pkt[0] = ATAPI_READ10;
+	pkt[1] = 0;
+	pkt[2] = (block >> 24) & 0xFF;
+	pkt[3] = (block >> 16) & 0xFF;
+	pkt[4] = (block >> 8) & 0xFF;
+	pkt[5] = block & 0xFF;
+	pkt[6] = 0;
+	pkt[7] = (sectors_to_read >> 8) & 0xFF;
+	pkt[8] = sectors_to_read & 0xFF;
+	pkt[9] = 0;
+	pkt[10] = 0;
+	pkt[11] = 0;
+
+	lock_resource(&ide->resource);
+	for(n = 0; n < sectors_to_read; n++, block++) {
+		for(retries = 0; retries < MAX_CD_ERR; retries++) {
+			if(send_packet_command(pkt, ide, drive, blksize)) {
+				printk("\tblock=%d, offset=%d\n", block, block * blksize);
+				unlock_resource(&ide->resource);
+				return -EIO;
+			}
+			if(atapi_read_data(dev, buffer, ide, drive, blksize, n * ATA_CD_SECTSIZE)) {
+				int errcode;
+				int sense_key;
+				errcode = inport_b(ide->base + ATA_ERROR);
+				sense_key = (errcode & 0xF0) >> 4;
+				printk("\tSense Key code indicates a '%s' condition.\n", sense_key_err[sense_key & 0xF]);
+				if(sense_key) {
+					continue;
+				}
+			}
+			break;
+		}
+		if(retries == MAX_CD_ERR) {
+			printk("\tblock=%d, offset=%d\n", block, block * blksize);
+			unlock_resource(&ide->resource);
+			return -EIO;
+		}
+
+	}
+	unlock_resource(&ide->resource);
+	return sectors_to_read * ATA_CD_SECTSIZE;
+}
+
+int ata_cd_ioctl(struct inode *i, int cmd, unsigned long int arg)
+{
+	struct ide *ide;
+
+	if(!(ide = get_ide_controller(i->rdev))) {
+		return -EINVAL;
+	}
+
+	switch(cmd) {
+		default:
+			return -EINVAL;
+			break;
+	}
+
+	return 0;
+}
+
+int ata_cd_init(struct ide *ide, struct ata_drv *drive)
+{
+	struct device *d;
+	unsigned char minor;
+
+	drive->fsop = &ata_cd_driver_fsop;
+
+	minor = !drive->minor_shift ? 0 : 1 << drive->minor_shift;
+
+	if(!(d = get_device(BLK_DEV, MKDEV(drive->major, minor)))) {
+		return -EINVAL;
+	}
+	SET_MINOR(d->minors, minor);
+	((unsigned int *)d->device_data)[minor] = CDROM_DEFAULT_SIZE;
+
+	return 0;
+}

commit 26b1c51df98d7179b56a8fdc61219507dbe3044e

fixed a regression introduced in b985ec1 that prevented from accessing ext2 filesystems with 4KB of block size

drivers/block/ata.c
@@ -423,9 +423,6 @@ static void show_capabilities(struct ide *ide, struct ata_drv *drive)
 			drive->multi = drive->ident.rw_multiple & 0xFF;
 			nrsectors = PAGE_SIZE / ATA_HD_SECTSIZE;
 			drive->multi = MIN(drive->multi, nrsectors);
-
-			/* FIXME: pending to rethink buffer cache */
-			drive->multi = 2;
 		}
 	}
 
@@ -454,6 +451,9 @@ static void show_capabilities(struct ide *ide, struct ata_drv *drive)
 		printk(", 16bit");
 	}
 	printk(", multi %d", drive->multi);
+	if(drive->flags & DRIVE_HAS_RW_MULTIPLE) {
+		printk("(%d)", drive->ident.rw_multiple & 0xFF);
+	}
 
 	if(drive->ident.capabilities & ATA_HAS_LBA) {
 		drive->flags |= DRIVE_REQUIRES_LBA;

drivers/block/ata_hd.c
@@ -115,8 +115,8 @@ int ata_hd_close(struct inode *i, struct fd *fd_table)
 int ata_hd_read(__dev_t dev, __blk_t block, char *buffer, int blksize)
 {
 	int minor;
-	int sectors_to_read, datalen;
-	int n, status, r, retries;
+	int sectors_to_read, datalen, nrsectors;
+	int n, status;
 	__off_t offset;
 	struct ide *ide;
 	struct ata_drv *drive;
@@ -132,8 +132,6 @@ int ata_hd_read(__dev_t dev, __blk_t block, char *buffer, int blksize)
 		minor &= ~(1 << IDE_SLAVE_MSF);
 	}
 
-	SET_ATA_RDY_RETR(retries);
-
 	blksize = blksize ? blksize : BLKSIZE_1K;
 	sectors_to_read = MIN(blksize, PAGE_SIZE) / ATA_HD_SECTSIZE;
 
@@ -142,15 +140,18 @@ int ata_hd_read(__dev_t dev, __blk_t block, char *buffer, int blksize)
 
 	if(drive->flags & DRIVE_HAS_RW_MULTIPLE) {
 		datalen = ATA_HD_SECTSIZE * sectors_to_read;
+		nrsectors = MIN(sectors_to_read, drive->multi);
 	} else {
 		datalen = ATA_HD_SECTSIZE;
+		nrsectors = 1;
 	}
 
 	CLI();
 	lock_resource(&ide->resource);
 
-	for(n = 0; n < sectors_to_read; n++) {
-		if(ata_io(ide, drive, offset, drive->multi)) {
+	n = 0;
+	while(n < sectors_to_read) {
+		if(ata_io(ide, drive, offset, nrsectors)) {
 			unlock_resource(&ide->resource);
 			return -EIO;
 		}
@@ -193,11 +194,9 @@ int ata_hd_read(__dev_t dev, __blk_t block, char *buffer, int blksize)
 		if(!(drive->flags & DRIVE_HAS_DMA)) {
 			drive->xfer.read_fn(ide->base + ATA_DATA, (void *)buffer, datalen / drive->xfer.copy_raw_factor);
 		}
-		if(drive->flags & DRIVE_HAS_RW_MULTIPLE) {
-			break;
-		}
-		offset++;
-		buffer += ATA_HD_SECTSIZE;
+		n += nrsectors;
+		offset += nrsectors;
+		buffer += (ATA_HD_SECTSIZE * nrsectors);
 	}
 	inport_b(ide->base + ATA_STATUS);	/* clear any pending interrupt */
 	unlock_resource(&ide->resource);
@@ -207,7 +206,7 @@ int ata_hd_read(__dev_t dev, __blk_t block, char *buffer, int blksize)
 int ata_hd_write(__dev_t dev, __blk_t block, char *buffer, int blksize)
 {
 	int minor;
-	int sectors_to_write, datalen;
+	int sectors_to_write, datalen, nrsectors;
 	int n, status, r, retries;
 	__off_t offset;
 	struct ide *ide;
@@ -234,15 +233,18 @@ int ata_hd_write(__dev_t dev, __blk_t block, char *buffer, int blksize)
 
 	if(drive->flags & DRIVE_HAS_RW_MULTIPLE) {
 		datalen = ATA_HD_SECTSIZE * sectors_to_write;
+		nrsectors = MIN(sectors_to_write, drive->multi);
 	} else {
 		datalen = ATA_HD_SECTSIZE;
+		nrsectors = 1;
 	}
 
 	CLI();
 	lock_resource(&ide->resource);
 
-	for(n = 0; n < sectors_to_write; n++) {
-		if(ata_io(ide, drive, offset, drive->multi)) {
+	n = 0;
+	while(n < sectors_to_write) {
+		if(ata_io(ide, drive, offset, nrsectors)) {
 			unlock_resource(&ide->resource);
 			return -EIO;
 		}
@@ -286,11 +288,9 @@ int ata_hd_write(__dev_t dev, __blk_t block, char *buffer, int blksize)
 			ata_stop_dma(ide, drive);
 		}
 #endif /* CONFIG_PCI */
-		if(drive->flags & DRIVE_HAS_RW_MULTIPLE) {
-			break;
-		}
-		offset++;
-		buffer += ATA_HD_SECTSIZE;
+		n += nrsectors;
+		offset += nrsectors;
+		buffer += (ATA_HD_SECTSIZE * nrsectors);
 	}
 	inport_b(ide->base + ATA_STATUS);	/* clear any pending interrupt */
 	unlock_resource(&ide->resource);