mikaku/Fiwix

commit 57d6d789a3dfb5eabadae154c490399b6126db2c

reorganized and improved the system console related code

kernel/main.c
@@ -31,6 +31,7 @@
 #include <fiwix/mm.h>
 #include <fiwix/ipc.h>
 #include <fiwix/kexec.h>
+#include <fiwix/sysconsole.h>
 
 int kparm_memsize;
 int kparm_extmemsize;
@@ -39,7 +40,7 @@ int kparm_ramdisksize;
 char kparm_rootfstype[10];
 char kparm_rootdevname[DEVNAME_MAX + 1];
 char kparm_initrd[DEVNAME_MAX + 1];
-int kparm_syscondev;
+int kparm_syscondev = 0;
 char kparm_bgaresolution[15];
 int kparm_ro;
 
@@ -59,16 +60,25 @@ struct new_utsname sys_utsname = {
 
 struct kernel_stat kstat;
 
+static void set_default_values(void)
+{
+	/* filesystem is ext2 */
+	strcpy(kparm_rootfstype, "ext2");
+
+	/* console is /dev/tty0 */
+	if(!kparm_syscondev) {
+		kparm_syscondev = MKDEV(VCONSOLES_MAJOR, 0);
+		add_sysconsoledev(kparm_syscondev);
+	}
+}
+
 void start_kernel(unsigned int magic, unsigned int info, unsigned int last_boot_addr)
 {
 	struct proc *init;
 
 	_last_data_addr = last_boot_addr - PAGE_OFFSET;
 	memset_b(&kstat, 0, sizeof(kstat));
-
-	/* default kernel values */
-	strcpy(kparm_rootfstype, "ext2");		/* filesystem is ext2 */
-	kparm_syscondev = MKDEV(VCONSOLES_MAJOR, 0);	/* console is /dev/tty0 */
+	sysconsole_init();
 
 #ifdef CONFIG_QEMU_DEBUGCON
 	if(inport_b(QEMU_DEBUG_PORT) == QEMU_DEBUG_PORT) {
@@ -90,6 +100,7 @@ void start_kernel(unsigned int magic, unsigned int info, unsigned int last_boot_
 
 	cpu_init();
 	multiboot(magic, info);
+	set_default_values();
 	pic_init();
 	irq_init();
 	idt_init();

drivers/char/console.c
@@ -22,6 +22,7 @@
 #include <fiwix/stdio.h>
 #include <fiwix/string.h>
 #include <fiwix/fbcon.h>
+#include <fiwix/sysconsole.h>
 
 #define CSI_J_CUR2END	0	/* clear from cursor to end of screen */
 #define CSI_J_STA2CUR	1	/* clear from start of screen to cursor */
@@ -966,30 +967,9 @@ void vconsole_deltab(struct tty *tty)
 	}
 }
 
-void console_flush_log_buf(char *buffer, unsigned int count)
-{
-	struct tty *tty;
-
-	if(!(tty = get_tty(kparm_syscondev))) {
-		kparm_syscondev = MKDEV(VCONSOLES_MAJOR, 0);
-		tty = get_tty(kparm_syscondev);
-	}
-
-	kstat.syscondev = kparm_syscondev;
-	while(count) {
-		if(tty_queue_putchar(tty, &tty->write_q, *buffer) < 0) {
-			tty->output(tty);
-			continue;
-		}
-		count--;
-		buffer++;
-	}
-	tty->output(tty);
-}
-
 void console_init(void)
 {
-	int n;
+	int syscon, n;
 	struct tty *tty;
 
 	if(video.flags & VPF_VGA) {
@@ -1052,7 +1032,20 @@ void console_init(void)
 	register_device(CHR_DEV, &console_device);
 	register_device(CHR_DEV, &tty_device);
 
-	if(is_vconsole(kparm_syscondev)) {
-		register_console(console_flush_log_buf);
+	/* check if a vconsole will act as a system console */
+	for(n = 0, syscon = 0; n < NR_SYSCONSOLES; n++) {
+		if(is_vconsole(sysconsole_table[n].dev)) {
+			tty = get_tty(sysconsole_table[n].dev);
+			/* this is required for the case of /dev/tty0 */
+			if(!syscon) {
+				syscon = tty->dev;
+			}
+			register_console(tty);
+		}
+	}
+	if(syscon) {
+		/* flush early log into the first console */
+		tty = get_tty(syscon);
+		flush_log_buf(tty);
 	}
 }

drivers/char/serial.c
@@ -19,6 +19,7 @@
 #include <fiwix/ctype.h>
 #include <fiwix/stdio.h>
 #include <fiwix/string.h>
+#include <fiwix/sysconsole.h>
 
 static struct fs_operations serial_driver_fsop = {
 	0,
@@ -677,7 +678,8 @@ static int serial_isa(void)
 
 void serial_init(void)
 {
-	int minor;
+	int minor, n, syscon;
+	struct tty *tty;
 
 	memset_b(serial_table, 0, sizeof(serial_table));
 
@@ -691,8 +693,21 @@ void serial_init(void)
 		if(register_device(CHR_DEV, &serial_device)) {
 			printk("WARNING: %s(): unable to register serial device.\n", __FUNCTION__);
 		}
-		if(is_serial(kparm_syscondev)) {
-			register_console(console_flush_log_buf);
+
+		/* check if a serial tty will act as a system console */
+		for(n = 0, syscon = 0; n < NR_SERIAL; n++) {
+			if(is_serial(sysconsole_table[n].dev)) {
+				if(!syscon) {
+					syscon = sysconsole_table[n].dev;
+				}
+				tty = get_tty(sysconsole_table[n].dev);
+				register_console(tty);
+			}
+		}
+		if(syscon) {
+			/* flush early log into the first console */
+			tty = get_tty(syscon);
+			flush_log_buf(tty);
 		}
 	}
 }

include/fiwix/config.h
@@ -32,6 +32,7 @@
 #define MAX_SPU_NOTICES		10	/* max. number of messages on spurious
 					   interrupts */
 #define RAMDISK_DRIVES		1	/* number of all-purpose ramdisk drives */
+#define NR_SYSCONSOLES		1	/* max. number of system consoles */
 
 
 /* toggle configuration options */

include/fiwix/kernel.h
@@ -81,7 +81,6 @@ struct kernel_stat {
 	int tz_dsttime;			/* type of DST correction */
 	unsigned int uptime;		/* seconds since boot */
 	unsigned int processes;		/* number of forks since boot */
-	int syscondev;			/* system console device */
 	int physical_pages;		/* physical memory (in pages) */
 	int kernel_reserved;		/* kernel memory reserved (in KB) */
 	int physical_reserved;		/* physical memory reserved (in KB) */

include/fiwix/kparms.h
@@ -25,14 +25,14 @@ static struct kparms parm_table[] = {
 	},
 #endif /* CONFIG_BGA */
 	{ "console=",
-	   { "/dev/tty1", "/dev/tty2", "/dev/tty3", "/dev/tty4", "/dev/tty5",
-	     "/dev/tty6", "/dev/tty7", "/dev/tty8", "/dev/tty9", "/dev/tty10",
-	     "/dev/tty11", "/dev/tty12",
+	   { "/dev/tty0", "/dev/tty1", "/dev/tty2", "/dev/tty3", "/dev/tty4",
+	     "/dev/tty5", "/dev/tty6", "/dev/tty7", "/dev/tty8", "/dev/tty9",
+	     "/dev/tty10", "/dev/tty11", "/dev/tty12",
 	     "/dev/ttyS0", "/dev/ttyS1", "/dev/ttyS2", "/dev/ttyS3"
 	   },
-	   { 0x401, 0x402, 0x403, 0x404, 0x405,
-	     0x406, 0x407, 0x408, 0x409, 0x40A,
-	     0x40B, 0x40C,
+	   { 0x400, 0x401, 0x402, 0x403, 0x404,
+	     0x405, 0x406, 0x407, 0x408, 0x409,
+	     0x40A, 0x40B, 0x40C,
 	     0x440, 0x441, 0x442, 0x443
 	   }
 	},

include/fiwix/stdio.h
@@ -8,7 +8,9 @@
 #ifndef _INCLUDE_STDIO_H
 #define _INCLUDE_STDIO_H
 
-void register_console(void (*fn)(char *, unsigned int));
+#include <fiwix/tty.h>
+
+void flush_log_buf(struct tty *);
 void printk(const char *, ...);
 int sprintk(char *, const char *, ...);
 

include/fiwix/sysconsole.h
@@ -1,0 +1,26 @@
+/*
+ * fiwix/include/fiwix/sysconsole.h
+ *
+ * Copyright 2024, Jordi Sanfeliu. All rights reserved.
+ * Distributed under the terms of the Fiwix License.
+ */
+
+#ifndef _FIWIX_SYSCONSOLE_H
+#define _FIWIX_SYSCONSOLE_H
+
+#include <fiwix/config.h>
+#include <fiwix/types.h>
+#include <fiwix/tty.h>
+
+struct sysconsole {
+	__dev_t dev;
+	struct tty *tty;
+};
+
+extern struct sysconsole sysconsole_table[NR_SYSCONSOLES];
+
+int add_sysconsoledev(__dev_t);
+void register_console(struct tty *);
+void sysconsole_init(void);
+
+#endif /* _FIWIX_SYSCONSOLE_H */

commit 8e45d5e960769b1d011e29d9d21f3f00476a912d

fixed a bug introduced in 57d6d78 that always assigned the default value (ext2) to 'rootfstype='

kernel/main.c
@@ -63,7 +63,9 @@ struct kernel_stat kstat;
 static void set_default_values(void)
 {
 	/* filesystem is ext2 */
-	strcpy(kparm_rootfstype, "ext2");
+	if(!kparm_rootfstype[0]) {
+		strcpy(kparm_rootfstype, "ext2");
+	}
 
 	/* console is /dev/tty0 */
 	if(!kparm_syscondev) {