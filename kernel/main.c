mikaku/Fiwix

commit 4317cbe3ce47258529054373bf21984abb15eea4

fixed the mechanism to sleep all processes in stop_kernel()

kernel/main.c
@@ -141,7 +141,7 @@ void start_kernel(unsigned long magic, unsigned long info, unsigned int stack)
 
 void stop_kernel(void)
 {
-	struct proc *p;
+	struct proc *p, *next;
 	int n;
 
 	/* stop and disable all interrupts! */
@@ -158,9 +158,10 @@ void stop_kernel(void)
 
 	/* put all processes to sleep and reset all pending signals */
 	FOR_EACH_PROCESS_RUNNING(p) {
+		next = p->next;
 		not_runnable(p, PROC_SLEEPING);
 		p->sigpending = 0;
-		p = p->next_run;
+		p = next;
 	}
 
 	/* enable keyboard only */

commit fb106e01c05bcbd43bbf741ac20ef74f167525c2

fixed a bug introduced in 4317cbe3ce47258529054373bf21984abb15eea4 that prevented from continuing to the next running process in stop_kernel()

kernel/main.c
@@ -167,7 +167,7 @@ void stop_kernel(void)
 
 	/* put all processes to sleep and reset all pending signals */
 	FOR_EACH_PROCESS_RUNNING(p) {
-		next = p->next;
+		next = p->next_run;
 		not_runnable(p, PROC_SLEEPING);
 		p->sigpending = 0;
 		p = next;